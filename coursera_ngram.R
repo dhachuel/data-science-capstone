c5190ea28cd45b5c9d5322b797ad51725032aa52
David Hachuel <dhachuel@users.noreply.github.com>
2017-03-05 15:01:49 -0500

    Week 4 enhancements.
    Still facing problems with memory usage.

--- coursera_ngram.R
+++ coursera_ngram.R
@@ -1,88 +1,22 @@
 library(quanteda)
-library(readtext)
-setwd("/home/dhachuel/Dropbox/Johns Hopkins Coursera/Capstone Project")  
-src_file <- "final/en_US/en_US.news.txt"
-
-crpus <- quanteda::corpus(x = readtext::readtext(file = src_file))
-
-tokens <- toLower(
-  quanteda::tokenize(crpus,
-    removeNumbers = T,
-    removePunct = T,
-    removeSeparators = T,
-    removeTwitter = T,
-    ngrams = 4,
-    concatenator = " ",
-    simplify = T
-  ) 
-)
-
-ngramSTR2Vector <- function(s){
-  unlist(strsplit(s, " "))
-}
-
-m_ngrams <- as.data.frame(matrix(
-  unlist(lapply(tokens, FUN=ngramSTR2Vector)), 
-  ncol = 4, 
-  byrow = TRUE
-))
-names(m_ngrams) <- c('input_1', 'input_2', 'input_3', 'input_4')
-
-
-input_text <- "I want to get a lot of good athletes as far"
-input_text <- tolower(input_text)
-input_words <- tail(tokenizers::tokenize_words(input_text)[[1]],3)
-sort(table(factor(m_ngrams[{
-  m_ngrams$input_1 == input_words[1] &
-  m_ngrams$input_2 == input_words[2] &
-    m_ngrams$input_3 == input_words[3]  
-},]$input_4)), decreasing = TRUE)[1:3]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-con <- file("final/en_US/en_US.news.txt", "r")
+library(tokenizers)
+setwd("C:/Users/n813863/Documents/CourseraCapstone")  
+src_file <- "data/final/en_US/en_US.news.txt"
+con <- file(src_file, "r")
 raw <-readLines(con)
 close(con)
+
 ##
 ## SETTINGS
 ##
-NGRAM_MAX <- 4
-NGRAM_MIN <- 2
+NGRAM_MAX <- 5
+NGRAM_MIN <- 5
 
-
-##
-## LOAN MODULES AND LIBRARIES
-##
-library(tokenizers)
-library(doParallel)
-library(foreach)
-
-##
-## SETUP CLUSTER
-##
-cl <- makeCluster(4)
-doParallel::registerDoParallel(cl)
 
 ##
 ## TOKENIZE
 ##
-raw.slice_len <- length(raw[1:200000])/4
-# sentences <- foreach(i = 0:3) %dopar% unlist(tokenizers::tokenize_sentences({
-#   raw[(raw.slice_len*i):(raw.slice_len*(i+1))]
-# }))
-sentences <- unlist(tokenizers::tokenize_sentences(raw[1:200000]))
+sentences <- unlist(tokenizers::tokenize_sentences(raw))
 rm(raw)
 gc()
 ngrams <- unlist(tokenizers::tokenize_ngrams(
@@ -106,38 +40,94 @@ ngramSTR2Vector <- function(s){
   unlist(strsplit(s, " "))
 }
   
-ngrams.v <- lapply(ngrams, FUN=ngramSTR2Vector)
+m_ngrams <- as.data.frame(matrix(
+  unlist(lapply(ngrams, FUN=ngramSTR2Vector)), 
+  ncol = NGRAM_MAX, 
+  byrow = TRUE
+))
+names(m_ngrams) <- c('input_1', 'input_2', 'input_3', 'input_4', 'input_5')
 rm(ngrams)
 gc()
 
 
 
-m_chain <- as.data.frame(matrix(unlist(ngrams.v), ncol = 4, byrow = TRUE))
-names(m_chain) <- c('input', 'output')
 
-unique_input_grams <- unique(m_chain[,1])
-unique_output_grams <- unique(m_chain[,2])
+input_text <- "When you breathe, I want to be the air for you. I'll be there for you, I'd live and I'd"
+test_words <- c('eat', 'give', 'die', 'sleep')
+# give WRONG -> die
+
+input_text <- "Guy at my table's wife got up to go to the bathroom and I asked about dessert and he started telling me about his"
+test_words <- c('financial', 'marital', 'horticultural', 'spiritual')
+# financial WRONG -> marital
+
+input_text <- "I'd give anything to see arctic monkeys this"
+test_words <- c('weekend', 'decade', 'month', 'morning')
+# month WRONG -> weekend
+
+input_text <- "Talking to your mom has the same effect as a hug and helps reduce your"
+test_words <- c('happiness', 'hunger', 'stress', 'sleepness')
+# hunger WRONG -> 
+
+input_text <- "When you were in Holland you were like 1 inch away from me but you hadn't time to take a"
+test_words <- c('look', 'minute', 'picture', 'walk')
+# minute
+
+input_text <- "I'd just like all of these questions answered, a presentation of evidence, and a jury to settle the"
+test_words <- c('incident', 'case', 'account', 'matter')
+# case WRONG -> matter
+
+input_text <- "I can't deal with unsymetrical things. I can't even hold an uneven number of bags of groceries in each"
+test_words <- c('arm', 'finger', 'toe', 'hand')
+# hand
+
+input_text <- "Every inch of you is perfect from the bottom to the"
+test_words <- c('center', 'side', 'top', 'middle')
+# top
+
+input_text <- "I’m thankful my childhood was filled with imagination and bruises from playing"
+test_words <- c('daily', 'outside', 'weekly', 'inside')
+# outside
+
+input_text <- "I like how the same people are in almost all of Adam Sandler's"
+test_words <- c('novels', 'pictures', 'movies', 'stories')
+# movies
+
+input_text <- tolower(input_text)
+input_words <- tail(tokenizers::tokenize_words(input_text)[[1]],4)
+result.all <- sort(table(factor(m_ngrams[{
+  {
+    m_ngrams$input_1 == input_words[1] &
+    m_ngrams$input_2 == input_words[2] &
+    m_ngrams$input_3 == input_words[3] &
+    m_ngrams$input_4 == input_words[4]
+  } |
+  {
+    m_ngrams$input_2 == input_words[2] &
+    m_ngrams$input_3 == input_words[3] &
+    m_ngrams$input_4 == input_words[4]
+  } |
+  {
+    m_ngrams$input_3 == input_words[3] &
+    m_ngrams$input_4 == input_words[4]
+  } |
+  {
+    m_ngrams$input_4 == input_words[4]
+  }
+    
+},]$input_5)), decreasing = TRUE)
+
+
+result.all <- sort(table(factor(m_ngrams[{
+  {
+      m_ngrams$input_4 == input_words[4]
+  }
+},]$input_5)), decreasing = TRUE)
+
+# is.element('morning', names(result.all))
+sort(result.all[test_words], decreasing=T)
 
 
 
-##
-## TRAIN MARKOV CHAIN
-##
-ngram_chain <- matrix(0, nrow = length(unique_input_grams), ncol = length(unique_output_grams))
-rownames(ngram_chain) <- unique_input_grams
-colnames(ngram_chain) <- unique_output_grams
-
-for(ngram in ngrams.v){
-  input_item = tolower(paste(ngram[1:length(ngram)-1], collapse=" "))
-  output_item = tolower(paste(tail(ngram, n=1), collapse=" "))
-  
-  ngram_chain[input_item, output_item] <- ngram_chain[input_item, output_item] + 1
-}
-
-##
-## MAKE ROW PERCENTS
-##
-ngram_chain.pct <- round(ngram_chain/rowSums(ngram_chain), 2)
 
 
 
@@ -147,43 +137,8 @@ ngram_chain.pct <- round(ngram_chain/rowSums(ngram_chain), 2)
 input_text <- "Lorem IpsUm"
 
 predict <- function(input_text){
-  input_text <- tolower(input_text)
-  input_words <- tokenizers::tokenize_words(input_text)[[1]]
-  
-  
-  input_slices <- c()
-  for(slice_size in seq(from = NGRAM_MIN-1, to = NGRAM_MAX-1, by = 1)){
-    input_slices <- c(input_slices, paste(tail(input_words, n = slice_size), collapse=" "))
-  }
-  input_slices <- unique(input_slices)
-  
-  
-  choices <- c()
-  for(input_slice in input_slices){
-    choices <- c(choices, sort(ngram_chain.pct[input_slice,], decreasing = TRUE)[1])
-  }
-  final_choice <- sort(choices, decreasing = TRUE)[1]
-  output <- names(final_choice)
-  
-  return(output)
 }
 
 predict(input_text = "Cras iaculis") # Cras iaculis tortor
 predict(input_text = "Cras iaculis tortor") # Cras iaculis tortor vitae
 predict(input_text = "habitant morbi tristique senectus et") # habitant morbi tristique senectus et netus
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
